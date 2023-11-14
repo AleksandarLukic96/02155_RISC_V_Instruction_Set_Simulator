f488584afd8d36c454ea5c4a36ee968f18727bb7
AleksandarLukic96 <60717693+AleksandarLukic96@users.noreply.github.com>
2023-11-14 11:52:12 +0100

    Created InstrunctionMemory Class with functions and fields. Additionally updated pc.py to handle the class and it's PC for the loop. Created decoder.py file

--- instMem.py
+++ instMem.py
@@ -1,65 +1,127 @@
-#  Implementation of instruction memory  
-
+#  Implementation of Instruction Memory Class and functions 
 import os
 
-# Get path to test file
-dir = os.getcwd()
-test_folder = "tests"
+# Instruction Memory Class
+class InstructionMemomry:
+    def __init__(self, file_path, pc = 0):
+        
+        # Load bin file into pyton
+        f = open(file_path, mode = "rb")
+        
+        # Reading file data with read() method
+        self.data = f.read()
+        
+        # Total number of instructions in program        
+        self.number_of_insts = len(self.data) // 4
+        
+        # Set Program Counter as parsed, otherwise initialise to 0
+        self.pc = pc
+        
+        # Concatinate bytes into 32-bit instructions as int-array
+        self.insts = []
+        
+        i = 0
+        while i < len(self.data):
+            self.insts.append(
+                (self.data[i+3] << 24) | 
+                (self.data[i+2] << 16) | 
+                (self.data[i+1] << 8) | 
+                self.data[i])
+            i += 4
+        
+        # Closing the opened file
+        f.close()   
+    
+    # Printing to console functions
+    def print_type(self):
+        print(type(self.data))
+    
+    def print_bytes(self):
+        print(f"{len(self.data)} bytes")
+        
+    def print_total_insts(self):
+        print(f"{self.number_of_insts} x 32-bit instructions")
+    
+    def print_insts_bin(self):
+        i = 0
+        for inst in self.insts:
+            str_bin = "{0:032b}".format(inst)
+            str_i = "{0:3}".format(i)
+            print(f"{str_i} Bin: {str_bin}")
+            i += 1
+    
+    def print_insts_hex(self):
+        i = 0
+        for inst in self.insts:
+            str_hex = "{0:08x}".format(inst)
+            str_i = "{0:3}".format(i)
+            print(f"{str_i} Hex: {str_hex}")
+            i += 1
+    
+    def print_insts_int(self):
+        i = 0
+        for inst in self.insts:
+            str_int = "{0:10}".format(inst)
+            str_i = "{0:3}".format(i)
+            print(f"{str_i} Int: {str_int}")
+            i += 1
+    
+    def print_insts(self):
+        # Print out program in binary, hex and decimal
+        i = 0
+        for inst in self.insts:
+            str_bin = "{0:032b}".format(inst)
+            str_hex = "{0:08x}".format(inst)
+            str_int = "{0:10}".format(inst)
+            str_opcode = "{0:07b}".format(inst & 127)
+            str_i = "{0:3}".format(i)
+            print(f"{str_i} Bin: {str_bin} Hex: {str_hex} Int: {str_int} Opcode: {str_opcode}")
+            i += 1
+    
 
-# Choose task
-task_1 = "task1"
-task_2 = "task2"
-task_3 = "task3"
-task_4 = "task4"
+# If file run as python file, test class functions
+if __name__ == "__main__":
 
-# Choose test file 
-file_names = [
-    "addlarge.bin"
-    ,"addneg.bin"
-    ,"addpos.bin"
-    ,"bool.bin"
-    ,"set.bin"
-    ,"shift.bin"
-    ,"shift2.bin"
-    ]
-file_name = file_names[0]
+    # Get path to test file
+    dir = os.getcwd()
+    test_folder = "tests"
 
-# Concat into full file path
-file_path = os.path.join(dir, test_folder, task_1, file_name)
+    # Choose task
+    task_1 = "task1"
+    task_2 = "task2"
+    task_3 = "task3"
+    task_4 = "task4"
 
+    # Choose test file 
+    file_names = [
+        "addlarge.bin"
+        ,"addneg.bin"
+        ,"addpos.bin"
+        ,"bool.bin"
+        ,"set.bin"
+        ,"shift.bin"
+        ,"shift2.bin"
+        ]
+    file_name = file_names[0]
 
-# Load bin file into pyton
-f = open(file_path, mode = "rb")
- 
-# Reading file data with read() method
-data = f.read()
- 
-# Knowing the Type and size of our data
-print(type(data))
-print(f"{len(data)} bytes")
-print(f"{len(data)//4} x 32-bit instructions")
-
-# Concatinate bytes into 32-bit instructions as int-array
-insts = []
-i = 0 
-while i < len(data):
-    insts.append(
-        (data[i+3] << 24) | 
-        (data[i+2] << 16) | 
-        (data[i+1] << 8) | 
-        data[i])
-    i = i + 4
-
-# Print out program in binary, hex and decimal
-i = 0
-for inst in insts:
-    str_bin = "{0:032b}".format(inst)
-    str_hex = "{0:08x}".format(inst)
-    str_int = "{0:10}".format(inst)
-    str_opcode = "{0:07b}".format(inst & 127)
-    str_i = "{0:3}".format(i)
-    print(f"{str_i} Bin: {str_bin} Hex: {str_hex} Int: {str_int} Opcode: {str_opcode}")
-    i = i + 1
-
-# Closing the opened file
-f.close()
+    # Concat into full file path
+    file_path = os.path.join(dir, test_folder, task_1, file_name)
+    print(f"file_path:\n{file_path}")
+    
+    imem = InstructionMemomry(file_path = file_path)
+    
+    print("\nimem.print_type()")
+    imem.print_type()
+    print("\nimem.print_bytes()")
+    imem.print_bytes()
+    print("\nimem.print_total_insts()")
+    imem.print_total_insts()
+    print("\nimem.print_insts_bin()")
+    imem.print_insts_bin()
+    print("\nimem.print_insts_hex()")
+    imem.print_insts_hex()
+    print("\nimem.print_insts_int()")
+    imem.print_insts_int()
+    print("\nimem.print_insts():")
+    imem.print_insts()
+    
--- pc.py
+++ pc.py
@@ -1,11 +1,24 @@
-import instMem as intr
+from instMem import InstructionMemomry as imem
 import decoder as dec
 
-pc=0
+# Initialise Program Counter to 0
+pc = 0
+
+# temp path for testing
+temp_path = "./tests/task1/addlarge.bin"
+
+# Initialise Instruciton Memory
+im = imem(file_path = temp_path, pc = pc)
+
 while True:
-    print(intr.insts[pc/4])
-    pc+=4
-    if((pc>>2)>=len(intr.insts)):
+    pc_str = "{0:3}".format(im.pc)
+    print(f"PC: {pc_str} Inst: {im.insts[im.pc//4]}")
+    
+    im.pc += 4
+    
+    if((im.pc >> 2) >= len(im.insts)):
         break
 
+# Print when exiting loop
+print("Done")
 
