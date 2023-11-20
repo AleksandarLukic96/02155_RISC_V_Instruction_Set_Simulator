dbc83bb330f980beff848ecf3e0d4764806fcf05
Aleksandar <aleksandar_lukic@outlook.com>
2023-11-20 11:57:59 +0100

    Added func3 and func7 to decoder.

--- src/decoder.py
+++ src/decoder.py
@@ -3,11 +3,16 @@
 # Decoder Class
 class Decoder:
     def __init__(self):
+        # Read 32-bit instruction
         self.inst = 0
+        
+        # Split instruction into parts for decoding
         self.opcode = 0
         self.reg_1 = 0
         self.reg_2 = 0
         self.rd = 0
+        self.func3 = 0
+        self.func7 = 0
         
     def set_inst(self, inst):
         self.inst = inst
@@ -38,9 +43,23 @@ class Decoder:
     
     def get_rd(self):
         return self.rd
+    
+    def set_func3(self, func3):
+        self.func3 = func3
+    
+    def get_func3(self):
+        return self.func3
+    
+    def set_func7(self, func7):
+        self.func7 = func7
+    
+    def get_func7(self):
+        return self.func7
 
     def compute_decoding(self):
         self.set_opcode(self.inst & 0b1111111)
         self.set_reg_1((self.inst >> 15) & 0b11111)
         self.set_reg_2((self.inst >> 20) & 0b11111)
         self.set_rd((self.inst >> 7) & 0b11111)
+        self.set_func3((self.inst >> 12) & 0b111)
+        self.set_func7((self.inst >> 25) & 0b1111111)
