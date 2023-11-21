bf7a498068e37687a2f0618c7657d0487a8ef9b9
Aleksandar <aleksandar_lukic@outlook.com>
2023-11-21 23:03:53 +0100

    Added placeholders for all the functions which need implementation in the ALU.

--- src/alu.py
+++ src/alu.py
@@ -40,37 +40,158 @@ class ALU:
     def get_res(self):
         return self.res
 
+    # R-type operations
+    def compute_add(self):
+        self.set_res(self.op_1 + self.op_2)
+
+    def compute_sub(self):
+        self.set_res(self.op_1 - self.op_2)
+    
+    def compute_xor(self):
+        self.set_res(self.op_1 ^ self.op_2)
+    
+    def compute_or(self):
+        self.set_res(self.op_1 | self.op_2)
+    
+    def compute_and(self):
+        self.set_res(self.op_1 & self.op_2)
+    
+    def compute_sll(self):
+        self.set_res(self.op_1 << self.op_2)
+    
+    def compute_srl(self):
+        self.set_res(self.op_1 >> self.op_2)
+    
+    def compute_sra(self):
+        self.set_res((self.op_1 >> self.op_2) if (self.op_1 >= 0) else ((self.op_1 + (1 << self.op_2) - 1) >> self.op_2))
+    
+    def compute_slt(self):
+        self.set_res(0 | (self.op_1 < self.op_2)) 
+    
+    def compute_sltu(self):
+        self.set_res(0 | (to_uint32(self.op_1) < to_uint32(self.op_2)))
+    
+    # I-type operations
+    def compute_addi(self):
+        pass
+    
+    def compute_xori(self):
+        pass
+    
+    def compute_ori(self):
+        pass
+    
+    def compute_andi(self):
+        pass
+    
+    def compute_slli(self):
+        pass
+    
+    def compute_srli(self):
+        pass
+    
+    def compute_srai(self):
+        pass
+    
+    def compute_slti(self):
+        pass
+    
+    def compute_sltiu(self):
+        pass
+    
+    # I-type load
+    def compute_lb(self):
+        pass
+    
+    def compute_lh(self):
+        pass
+    
+    def compute_lw(self):
+        pass
+    
+    def compute_lbu(self):
+        pass
+    
+    def compute_lhu(self):
+        pass
+    
+    
     def compute_res(self):
-        
+        # R-type
         if self.get_ctrl() == const.ADD:
-            self.set_res(self.op_1 + self.op_2)
-
-        elif self.get_ctrl() == const.SLL:
-            self.set_res(self.op_1 << self.op_2)
-
-        elif self.get_ctrl() == const.XOR:
-            self.set_res(self.op_1 ^ self.op_2)
-
-        elif self.get_ctrl() == const.OR:
-            self.set_res(self.op_1 | self.op_2)
-
-        elif self.get_ctrl() == const.AND:
-            self.set_res(self.op_1 & self.op_2)
-
-        elif self.get_ctrl() == const.SRL:
-            self.set_res(self.op_1 >> self.op_2)
+            self.compute_add()
 
         elif self.get_ctrl() == const.SUB:
-            self.set_res(self.op_1 - self.op_2)                        
+            self.compute_sub() 
+        
+        elif self.get_ctrl() == const.XOR:
+            self.compute_xor()
+
+        elif self.get_ctrl() == const.OR:
+            self.compute_or()
+        
+        elif self.get_ctrl() == const.AND:
+            self.compute_and()
+
+        elif self.get_ctrl() == const.SLL:
+            self.compute_sll()
+
+        elif self.get_ctrl() == const.SRL:
+            self.compute_srl()                       
 
         elif self.get_ctrl() == const.SRA:
-            self.set_res((self.op_1 >> self.op_2) if (self.op_1 >= 0) else ((self.op_1 + (1 << self.op_2) - 1) >> self.op_2))
+            self.compute_sra()
 
         elif self.get_ctrl() == const.SLT: 
-            self.set_res(0 | (self.op_1 < self.op_2)) 
+            self.compute_slt()
 
         elif self.get_ctrl() == const.SLTU: 
-            self.set_res(0 | (to_uint32(self.op_1) < to_uint32(self.op_2)))
+            self.compute_sltu()
 
+        # I-type
+        elif self.get_ctrl() == const.ADDI: 
+            self.compute_addi()
+        
+        elif self.get_ctrl() == const.XORI: 
+            self.compute_xori()
+        
+        elif self.get_ctrl() == const.ORI: 
+            self.compute_ori()
+        
+        elif self.get_ctrl() == const.ANDI: 
+            self.compute_andi()
+        
+        elif self.get_ctrl() == const.SLLI: 
+            self.compute_slli()
+        
+        elif self.get_ctrl() == const.SRLI: 
+            self.compute_srli()
+        
+        elif self.get_ctrl() == const.SRAI: 
+            self.compute_srai()
+        
+        elif self.get_ctrl() == const.SLTI: 
+            self.compute_slti()
+        
+        elif self.get_ctrl() == const.SLTIU: 
+            self.compute_sltiu()
+        
+        # I-type load
+        elif self.get_ctrl() == const.LB: 
+            self.compute_lb()
+        
+        elif self.get_ctrl() == const.LH: 
+            self.compute_lh()
+        
+        elif self.get_ctrl() == const.LW: 
+            self.compute_lw()
+        
+        elif self.get_ctrl() == const.LBU: 
+            self.compute_lbu()
+        
+        elif self.get_ctrl() == const.LHU: 
+            self.compute_lhu()
+        
+        # Unsupported operations
         else:
             pass
