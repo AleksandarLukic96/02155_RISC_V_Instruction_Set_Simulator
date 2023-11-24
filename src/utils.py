96169ce5932464b9aa8e20bd79cfe0858296507b
Aleksandar <aleksandar_lukic@outlook.com>
2023-11-24 21:14:58 +0100

    Added a remove_overflow function to enforce 32 bit values in python.

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,6 @@ def to_str_int(val):
 def to_str_int_hex_bin(val):
     return to_str_int(val) + " " + to_str_hex(val) + " " + to_str_bin(val)
 
-
 def sign_extend(num, n_bits = 16):
     n_bits = n_bits
     
@@ -44,45 +43,5 @@ def sign_extend(num, n_bits = 16):
     res = sign_extension_mask | num
     return res
 
-if __name__ == "__main__":
-    
-    n1 = 0x7FFFFFFF # 2147483647
-    n2 = 0x00000001 # 1
-    res = 0         # -2147483648
-    n1_ex = sign_extend(n1, n_bits = 32)
-    n2_ex = sign_extend(n2, n_bits = 32)
-    
-    print(f"n1: {n1}, n1_ex: {n1_ex}")
-    print(f"n2: {n2}, n2_ex: {n2_ex}")
-    
-    n3 = n1 + n2
-    n3_ex = n1_ex + n2_ex
-    print(f"n3: {n3}, n3_ex: {n3_ex}")
-    
-    n4 = sign_extend(n3_ex)
-    print(f"n4: {n4}")
-    
-    n4_ex = n4 & 0x0FFFFFFFF # <------ Always and after and operation.
-    print(f"n4_ex: {n4_ex}")
-    
-    # def f_hexconversion(num):
-    #     if num < 10:
-    #         return str(num)
-    #     elif num == 10:
-    #         return 'A'
-    #     elif num == 11:
-    #         return 'B'
-    #     elif num == 12:
-    #         return 'C'
-    #     elif num == 13:
-    #         return 'D'
-    #     elif num == 14:
-    #         return 'E'
-    #     elif num == 15:
-    #         return 'F'
-    
-    # def f_decToHex2(num):
-    #     if num == 0:
-    #         return ''
-    #     return f_decToHex2(num // 16) + f_hexconversion(num % 16)
-    
+def remove_overflow(num):
+    return num & 0x00000000FFFFFFFF
