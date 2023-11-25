8cc89616e59114eaac320c8e0415094d55b6bb71
Aleksandar <aleksandar_lukic@outlook.com>
2023-11-25 20:46:43 +0100

    Restructured the data memory to work with bytes instead of 32-bit blocks. Still undergoing testing.

--- src/dataMemory.py
+++ src/dataMemory.py
@@ -13,13 +13,10 @@ GiB = 1073741824
 
 # Instruction Memory Class
 class DataMemory:
-    def __init__(self, mem_size = MiB, file_path = "NO_FILE_GIVEN", addr = 0, little_endian = True):
+    def __init__(self, mem_size = MiB, file_path = "NO_FILE_GIVEN", little_endian = True):
         # Initialise size of memory
         self.mem_size = mem_size
-        
-        # Initialise empty memory with indicies of bitdepth 4 bytes
-        self.mem_addrs = [0] * (self.mem_size // 4)
-        
+    
         # Load bin file into pyton
         f = open(file_path, mode = "rb")
         
@@ -30,40 +27,23 @@ class DataMemory:
         self.little_endian = little_endian
         
         # Concatinate bytes into 32-bit instructions as int-array
-        self.mem_slot = []
+        self.mem_addrs = [0] * self.mem_size # <--- TODO: Rename to mem_addrs 
         
+        # Stored as Little endian [LSB ----> MSB]        
         i = 0   
         if self.little_endian:
             while i < (len(self.data)):
-                self.mem_slot.append(
-                    (self.data[i + 3] << 24) | 
-                    (self.data[i + 2] << 16) | 
-                    (self.data[i + 1] <<  8) | 
-                    self.data[i])
-                i += 4
-        else:        
-            while i < (len(self.data)):
-                self.mem_slot.append(
-                    (self.data[i] << 24) | 
-                    (self.data[i + 1] << 16) | 
-                    (self.data[i + 2] <<  8) | 
-                    self.data[i + 3])
-                i += 4
-
-        i = 0   
-        if self.little_endian:
-            while i < (len(self.data)):
-                self.mem_slot.append(self.data[i + 0])
-                self.mem_slot.append(self.data[i + 1])
-                self.mem_slot.append(self.data[i + 2])
-                self.mem_slot.append(self.data[i + 3])
+                self.mem_addrs[i + 0] = self.data[i + 0]
+                self.mem_addrs[i + 1] = self.data[i + 1]
+                self.mem_addrs[i + 2] = self.data[i + 2]
+                self.mem_addrs[i + 3] = self.data[i + 3]
                 i += 4
         else:
             while i < (len(self.data)):
-                self.mem_slot.append(self.data[i + 3])
-                self.mem_slot.append(self.data[i + 2])
-                self.mem_slot.append(self.data[i + 1])
-                self.mem_slot.append(self.data[i + 0])
+                self.mem_addrs[i + 0] = self.data[i + 3]
+                self.mem_addrs[i + 1] = self.data[i + 2]
+                self.mem_addrs[i + 2] = self.data[i + 1]
+                self.mem_addrs[i + 3] = self.data[i + 0]
                 i += 4
         
         # Closing the opened file
@@ -77,7 +57,7 @@ class DataMemory:
         # Load/Save instruction signal
         self.inst_signal = 0
         
-        # Offset from address
+        # Offset from address byte/halfword/word
         self.offset = 0
         
         # Write Enabled signal
@@ -141,42 +121,69 @@ class DataMemory:
         self.set_read_enabled(0)
     
     def write_to_addr(self):
+        # split 32-bit input data into 4 bytes in Little endian order
+        data_0 = (self.data_in & 0x000000FF) >>  0
+        data_1 = (self.data_in & 0x0000FF00) >>  8
+        data_2 = (self.data_in & 0x00FF0000) >> 16
+        data_3 = (self.data_in & 0xFF000000) >> 24
+        
         if self.get_write_enabled() == 1:
             # Write data_in to address in memory
             if self.inst_signal == const.SB:
-                    byte_value = utils.sign_extend((self.data_in & 0x000000FF), 8)
-                    extend_value = byte_value >> 24
-                    self.mem_addrs[self.addr] = byte_value
-                    self.mem_addrs[self.addr] = byte_value
-                    
+                self.mem_addrs[self.addr + 1*self.offset + 0] = data_0
+            
             elif self.inst_signal == const.SH:
-                self.mem_addrs[self.addr] = utils.sign_extend((self.data_in & 0x0000FFFF), 16)
+                self.mem_addrs[self.addr + 2*self.offset + 0] = data_0
+                self.mem_addrs[self.addr + 2*self.offset + 1] = data_1
+            
             elif self.inst_signal == const.SW:
-                self.mem_addrs[self.addr] = self.data_in
+                self.mem_addrs[self.addr + 4*self.offset + 0] = data_0
+                self.mem_addrs[self.addr + 4*self.offset + 1] = data_1
+                self.mem_addrs[self.addr + 4*self.offset + 2] = data_2
+                self.mem_addrs[self.addr + 4*self.offset + 3] = data_3
+                
             else:
                 pass
             
     def read_from_addr(self):
         if self.get_read_enabled() == 1:
-            # Read data_out from address in memory
+            # Read 4 bytes from memory in Little endian and parse into output data order 32-bit data
+
             if self.inst_signal == const.LB:
-                self.data_out = utils.sign_extend(self.mem_addrs[self.addr][self.get_offset()], 8) 
+                data_0 = self.mem_addrs[self.addr + 1*self.offset + 0]
+                self.data_out = utils.sign_extend(data_0, 8)
+                
             elif self.inst_signal == const.LH:
-                self.data_out = utils.sign_extend((self.mem_addrs[self.addr] & 0x0000FFFF), 16)
+                data_0 = self.mem_addrs[self.addr + 2*self.offset + 0]
+                data_1 = self.mem_addrs[self.addr + 2*self.offset + 1]
+                self.data_out = utils.sign_extend(
+                      (data_0 << 0)
+                    | (data_1 << 8)
+                    , 16)
+                
             elif self.inst_signal == const.LW:
-                self.data_out = self.mem_addrs[self.addr]
+                data_0 = self.mem_addrs[self.addr + 2*self.offset + 0]
+                data_1 = self.mem_addrs[self.addr + 2*self.offset + 1]
+                data_2 = self.mem_addrs[self.addr + 1*self.offset + 2]
+                data_3 = self.mem_addrs[self.addr + 1*self.offset + 3]
+                self.data_out = utils.sign_extend(
+                      (data_0 <<  0)
+                    | (data_1 <<  8)
+                    | (data_2 << 16)
+                    | (data_3 << 24)
+                    , 32)
+                
             elif self.inst_signal == const.LBU:
-                self.data_out = (self.mem_addrs[self.addr] & 0xFF000000) >> 24
+                data_0 = self.mem_addrs[self.addr + 1*self.offset + 0]
+                self.data_out = data_0
+                
             elif self.inst_signal == const.LHU:
-                self.data_out = (self.mem_addrs[self.addr] & 0xFFFF0000) >> 16
+                data_0 = self.mem_addrs[self.addr + 2*self.offset + 0]
+                data_1 = self.mem_addrs[self.addr + 2*self.offset + 1]
+                self.data_out = (data_0 << 0) | (data_1 << 8)
+            
             else:
                 pass
 
     def __repr__(self):
         return "addr: %s, data_in: %s, data_out: %s, read_enabled: %s, write_enabled: %s" % (self.get_addr(), self.get_data_in(), self.get_data_out(), self.get_read_enabled(), self.get_write_enabled())
-
-# If file is run as python file, test class functions
-if __name__ == "__main__":
-    file_path = "C:/Users/Aleksandar/02155_RISC_V_Instruction_Set_Simulator/tests/bin_files_only/_t3_width.bin"
-    dm = DataMemory(file_path = file_path)
-    print(dm.mem_addrs[dm.addr][dm.get_offset()])
