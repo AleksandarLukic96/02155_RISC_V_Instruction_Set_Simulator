421d3c88f8a5f28d6c77a95fa55ac2b7480aaefb
Aleksandar <aleksandar_lukic@outlook.com>
2023-11-26 23:13:07 +0100

    Removed print_fields from mux.

--- src/mux.py
+++ src/mux.py
@@ -34,14 +34,6 @@ class Mux2:
     
     def __repr__(self):
         return "in_0: %s, in_1: %s, select: %s, out: %s" % (self.get_in_0(), self.get_in_1(), self.get_select(), self.get_out())
-
-    def print_fields(self):
-        print(f"MUX2:")
-        print(f" in_0   : {self.get_in_0()}")
-        print(f" in_1   : {self.get_in_1()}")
-        print(f" select : {self.get_select()}")
-        print(f" out    : {self.get_out()}")
-        print() 
     
 # Multiplexor(Mux 3-to-1) Class 
 class Mux3:
@@ -91,12 +83,3 @@ class Mux3:
     
     def __repr__(self):
         return "in_0: %s, in_1: %s, in_2: %s, select: %s, out: %s" % (self.get_in_0(), self.get_in_1(), self.get_in_2(), self.get_select(), self.get_out())
-        
-    def print_fields(self):
-        print(f"MUX3:")
-        print(f" in_0   : {self.get_in_0()}")
-        print(f" in_1   : {self.get_in_1()}")
-        print(f" in_2   : {self.get_in_2()}")
-        print(f" select : {self.get_select()}")
-        print(f" out    : {self.get_out()}")
-        print()
